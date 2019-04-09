353b6f57d7f27424a74930f5693d9aa6b8cede27
michael <mbriningstool@gmail.com>
2019-04-09 13:06:39 -0400

    Commented out gui for testing of single return

--- coding.py
+++ coding.py
@@ -1,36 +1,35 @@
+
 #################################################################
 #iris-recognition original code base was created by
 #github user mokosaur https://github.com/mokosaur/iris-recognition
 #code was forked from original on 4-4-2019
 ################################################################
-
 import math
 import numpy as np
 
 from skimage.util import view_as_blocks
 
-def polar2cart( radius , xCoordinate , yCoordinate , polarAngle ):
-    """Changes polar coordinates to cartesian coordinate system.
 
+def polar2cart(radius, xCoordinate, yCoordinate, polarAngle):
+    """Changes polar coordinates to cartesian coordinate system.
+    :param radius: Radius
     :param xCoordinate: x coordinate of the origin
     :param yCoordinate: y coordinate of the origin
-    :param theta: Angle
+    :param polarAngle: Angle
     :return: Cartesian coordinates
     :rtype: tuple (int, int)
     """
-    xInCartesian = int( xCoordinate + radius * math.cos( polarAngle ) )
-    yInCartesian = int( yCoordinate + radius * math.sin( polarAngle ) )
+    x = int(xCoordinate + radius * math.cos(polarAngle))
+    y = int(yCoordinate + radius * math.sin(polarAngle))
+    return x, y
 
-    return xInCartesian , yInCartesian
 
-
-def unravel_iris(imageOfEye, centerOfPupilXCoordinate, centerOfPupilYPupilCoordinate, pupilRadius, xi, yi, ri, phase_width=300, iris_width=150):
-    """Unravels the iris from the image and transforms it into a rectangular representation.
-
-    :param imageOfEye: Image of an eye
-    :param centerOfPupilXCoordinate: x coordinate of the pupil centre
-    :param centerOfPupilYPupilCoordinate: y coordinate of the pupil centre
-    :param pupilRadius: Radius of the pupil
+def unravel_iris(img, xp, yp, rp, xi, yi, ri, phase_width=300, iris_width=150):
+    """Unravels the iris from the image and transforms it to a straightened representation.
+    :param img: Image of an eye
+    :param xp: x coordinate of the pupil centre
+    :param yp: y coordinate of the pupil centre
+    :param rp: Radius of the pupil
     :param xi: x coordinate of the iris centre
     :param yi: y coordinate of the iris centre
     :param ri: Radius of the iris
@@ -39,20 +38,17 @@ def unravel_iris(imageOfEye, centerOfPupilXCoordinate, centerOfPupilYPupilCoordi
     :return: Straightened image of the iris
     :rtype: ndarray
     """
-    if imageOfEye.ndim > 2:
-        imageOfEye = imageOfEye[:, :, 0].copy()
-
+    if img.ndim > 2:
+        img = img[:, :, 0].copy()
     iris = np.zeros((iris_width, phase_width))
-
     theta = np.linspace(0, 2 * np.pi, phase_width)
-    
     for i in range(phase_width):
-        begin = polar2cart(pupilRadius, centerOfPupilXCoordinate, centerOfPupilYPupilCoordinate, theta[i])
+        begin = polar2cart(rp, xp, yp, theta[i])
         end = polar2cart(ri, xi, yi, theta[i])
         xspace = np.linspace(begin[0], end[0], iris_width)
         yspace = np.linspace(begin[1], end[1], iris_width)
-        iris[:, i] = [255 - imageOfEye[int(y), int(x)]
-                      if 0 <= int(x) < imageOfEye.shape[1] and 0 <= int(y) < imageOfEye.shape[0]
+        iris[:, i] = [255 - img[int(y), int(x)]
+                      if 0 <= int(x) < img.shape[1] and 0 <= int(y) < img.shape[0]
                       else 0
                       for x, y in zip(xspace, yspace)]
     return iris
@@ -60,7 +56,6 @@ def unravel_iris(imageOfEye, centerOfPupilXCoordinate, centerOfPupilYPupilCoordi
 
 def gabor(rho, phi, w, theta0, r0, alpha, beta):
     """Calculates gabor wavelet.
-
     :param rho: Radius of the input coordinates
     :param phi: Angle of the input coordinates
     :param w: Gabor wavelet parameter (see the formula)
@@ -74,38 +69,36 @@ def gabor(rho, phi, w, theta0, r0, alpha, beta):
            np.exp(-(phi - theta0) ** 2 / beta ** 2)
 
 
-def gabor_convolve(imageOfEye, w, alpha, beta):
+def gabor_convolve(img, w, alpha, beta):
     """Uses gabor wavelets to extract iris features.
-
-    :param imageOfEye: Image of an iris
+    :param img: Image of an iris
     :param w: w parameter of Gabor wavelets
     :param alpha: alpha parameter of Gabor wavelets
     :param beta: beta parameter of Gabor wavelets
     :return: Transformed image of the iris (real and imaginary)
     :rtype: tuple (ndarray, ndarray)
     """
-    rho = np.array([np.linspace(0, 1, imageOfEye.shape[0]) for i in range(imageOfEye.shape[1])]).T
-    x = np.linspace(0, 1, imageOfEye.shape[0])
-    y = np.linspace(-np.pi, np.pi, imageOfEye.shape[1])
+    rho = np.array([np.linspace(0, 1, img.shape[0]) for i in range(img.shape[1])]).T
+    x = np.linspace(0, 1, img.shape[0])
+    y = np.linspace(-np.pi, np.pi, img.shape[1])
     xx, yy = np.meshgrid(x, y)
-    return rho * imageOfEye * np.real(gabor(xx, yy, w, 0, 0.5, alpha, beta).T), \
-           rho * imageOfEye * np.imag(gabor(xx, yy, w, 0, 0.5, alpha, beta).T)
+    return rho * img * np.real(gabor(xx, yy, w, 0, 0.5, alpha, beta).T), \
+           rho * img * np.imag(gabor(xx, yy, w, 0, 0.5, alpha, beta).T)
 
 
-def iris_encode(imageOfEye, dr=15, dtheta=15, alpha=0.4):
+def iris_encode(img, dr=15, dtheta=15, alpha=0.4):
     """Encodes the straightened representation of an iris with gabor wavelets.
-
-    :param imageOfEye: Image of an iris
+    :param img: Image of an iris
     :param dr: Width of image patches producing one feature
     :param dtheta: Length of image patches producing one feature
     :param alpha: Gabor wavelets modifier (beta parameter of Gabor wavelets becomes inverse of this number)
     :return: Iris code and its mask
     :rtype: tuple (ndarray, ndarray)
     """
-    # mean = np.mean(imageOfEye)
-    # std = imageOfEye.std()
-    mask = view_as_blocks(np.logical_and(100 < imageOfEye, imageOfEye < 230), (dr, dtheta))
-    norm_iris = (imageOfEye - imageOfEye.mean()) / imageOfEye.std()
+    # mean = np.mean(img)
+    # std = img.std()
+    mask = view_as_blocks(np.logical_and(100 < img, img < 230), (dr, dtheta))
+    norm_iris = (img - img.mean()) / img.std()
     patches = view_as_blocks(norm_iris, (dr, dtheta))
     code = np.zeros((patches.shape[0] * 3, patches.shape[1] * 2))
     code_mask = np.zeros((patches.shape[0] * 3, patches.shape[1] * 2))
@@ -125,7 +118,7 @@ def iris_encode(imageOfEye, dr=15, dtheta=15, alpha=0.4):
 if __name__ == '__main__':
     import cv2
     from datasets import load_utiris
-    import matplotlib.pyplot as plt
+    #import matplotlib.pyplot as plt
 
     data = load_utiris()['data']
     image = cv2.imread(data[0])
@@ -133,10 +126,11 @@ if __name__ == '__main__':
     iris = unravel_iris(image, 444, 334, 66, 450, 352, 245)
     code, mask = iris_encode(iris)
 
-    plt.subplot(211)
-    plt.imshow(iris, cmap=plt.cm.gray)
-    plt.subplot(223)
-    plt.imshow(code, cmap=plt.cm.gray, interpolation='none')
-    plt.subplot(224)
-    plt.imshow(mask, cmap=plt.cm.gray, interpolation='none')
-    plt.show()
+    #plt.subplot(211)
+    #plt.imshow(iris, cmap=plt.cm.gray)
+    #plt.subplot(223)
+    #plt.imshow(code, cmap=plt.cm.gray, interpolation='none')
+    #plt.subplot(224)
+
+    #plt.imshow(mask, cmap=plt.cm.gray, interpolation='none')
+#plt.show()
